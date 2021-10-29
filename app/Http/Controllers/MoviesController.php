878defebae6cdbbccddd43ae5d7582fc659c976d
bizoon <r.aureli@protonmail.com>
2021-10-29 13:41:02 +0100

    Stampo tutto il db con il dd

--- app/Http/Controllers/MoviesController.php
+++ app/Http/Controllers/MoviesController.php
@@ -3,8 +3,16 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Movies;
 
 class MoviesController extends Controller
 {
-    //
+    public function index(){
+    
+    $movies = Movies::all();
+    dd($movies);
+    return view('welcome');
+
+}
+
 }
